7027a6c3644890475bc39624912623d7af53e8be
crab85193 <crab85193@gmail.com>
2021-12-07 00:59:53 +0900

    [Add] Add LivingThing Class
    Added the LivingThing class and fixed the code for Hero and Enemy.

--- app/src/main/java/jp/ac/uryukyu/ie/e215726/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215726/Enemy.java
@@ -2,18 +2,9 @@ package jp.ac.uryukyu.ie.e215726;
 
 /**
  * 敵クラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
-
+public class Enemy extends LivingThing{
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
      * @param name モンスター名
@@ -21,24 +12,7 @@ public class Enemy {
      * @param attack モンスターの攻撃力
      */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
-
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        if( !dead ){
-            int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
+        super(name, maximumHP, attack);
     }
 
     /**
@@ -48,18 +22,10 @@ public class Enemy {
      */
     public void wounded(int damage){
         hitPoint -= damage;
-        if( hitPoint < 0 ) {
+        if( hitPoint <= 0 ) {
             dead = true;
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
-    }
-
-    /**
-     * 名前を取得するメソッド。
-     * @return モンスターの名前
-     */
-    public String getName(){
-        return name;
     }
 
     /**
@@ -76,14 +42,6 @@ public class Enemy {
      */
     public int getAttack(){
         return attack;
-    }
-
-    /**
-     * 生存状態を取得するメソッド。
-     * @return モンスターの生存状態
-     */
-    public boolean isDead(){
-        return dead;
     }
 
     /**
